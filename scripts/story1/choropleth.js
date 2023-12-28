b9ca3aae5a5e13d2c8ce41f76dbe8563e57d3bb0
VittorioBartolomeoSecondin <73950586+VittorioBartolomeoSecondin@users.noreply.github.com>
2023-12-28 18:42:20 +0100

    Update choropleth.js

--- scripts/story1/choropleth.js
+++ scripts/story1/choropleth.js
@@ -18,7 +18,7 @@ const Choropleth = {
 					.rotate([-20.0, 0.0])
 				        .center([0.0, 52.0])
 				        .parallels([35.0, 65.0])
-				        .translate([width / 2, height / 2 + 300])
+				        .translate([width / 2, height / 2 + 350])
 				        .scale(width)
 				        .precision(.1)
 		                   //.scale(width - 20)
