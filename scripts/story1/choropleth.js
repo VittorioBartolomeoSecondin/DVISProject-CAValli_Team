cb7d3753282916d6935148601c9de02694b7383e
VittorioBartolomeoSecondin <73950586+VittorioBartolomeoSecondin@users.noreply.github.com>
2023-12-28 19:24:14 +0100

    Update choropleth.js

--- scripts/story1/choropleth.js
+++ scripts/story1/choropleth.js
@@ -1,58 +1,32 @@
 const Choropleth = {
-	initialize: function() {
-		function updateMap(yearIndex) {
-		// set the dimensions and margins of the graph
-		var margin = { top: 60, right: 70, bottom: 70, left: 100 },
-		    width = 1435 - margin.left - margin.right,
-		    height = 900 - margin.top - margin.bottom;
+	createMap: function() {
+	   // set the dimensions and margins of the graph
+	   var margin = { top: 60, right: 70, bottom: 70, left: 100 },
+	       width = 1435 - margin.left - margin.right,
+	       height = 900 - margin.top - margin.bottom;
 		
-		// Define the width and height of the legend
-		const legendWidth = 110;
-		const legendHeight = 300;
+	   // Define the width and height of the legend
+	   const legendWidth = 110;
+	   const legendHeight = 300;
+	
+	   // Calculate the position for the legend
+	   const legendX = width - legendWidth - 80; 
+	   const legendY = height / 2 - legendHeight / 2 - 300; 
 		
-		// Calculate the position for the legend
-		const legendX = width - legendWidth - 80; 
-		const legendY = height / 2 - legendHeight / 2 - 300; 
-		
-		let projection = d3.geoAlbers()
-					.rotate([-20.0, 0.0])
-				        .center([0.0, 52.0])
-				        .parallels([35.0, 65.0])
-				        .translate([width / 2, height / 2 + 400])
-				        .scale(width)
-				        .precision(.1)
-		                   //.scale(width - 20)
-		                   //.translate([width / 2, height / 2]);
-		
-		// add tooltip
-		let tooltip = null;
-		
-		let mouseOver = function(event, d) {
-					d3.selectAll(".Country")
-						.transition()
-						.duration(200)
-						.style("opacity", .3)
-						.style("stroke", "black")
-						.style("stroke-width", "0.75px");
-					d3.select(this)
-						.transition()
-						.duration(200)
-						.style("opacity", 1)
-						.style("stroke-width", "2px");
-					// Create the tooltip if it doesn't exist
-				            if (!tooltip) {
-				                tooltip = d3.select("body").append("div")
-				                    .attr("class", "tooltip")
-				                    .style("opacity", 0);
-				            }
-					tooltip.html(d.properties.name + ' &#40;' + d.properties.abbreviation + '&#41;: ' + d.properties.abundance1000[yearIndex] + ' k NEETs')
-						.style("left", (event.pageX + 15) + "px")
-						.style("top", (event.pageY - 28) + "px")
-						.transition().duration(400)
-						.style("opacity", 1);
-				}
-		
-		let mouseLeave = function() {
+	   let projection = d3.geoAlbers()
+			      .rotate([-20.0, 0.0])
+			      .center([0.0, 52.0])
+			      .parallels([35.0, 65.0])
+			      .translate([width / 2, height / 2 + 400])
+			      .scale(width)
+			      .precision(.1);
+	   
+	   // add tooltip
+	   let tooltip = null;
+
+	   let mouseOver = null;
+
+	   let mouseLeave = function() {
 					d3.selectAll(".Country")
 						.transition()
 						.duration(200)
@@ -65,40 +39,40 @@ const Choropleth = {
 				                tooltip = null; // Reset tooltip variable
 		            	       }
 				}
-		
-		let path = d3.geoPath().projection(projection);
-		
-		// Define color scale
-		const colorScale = d3.scaleThreshold()
-			.domain([100000, 200000, 500000, 1000000, 1500000]) 
-			.range(d3.schemeOranges[6]);
-		
-		let svg = d3.select("#choropleth")
-			    .append("svg")
-			    .attr("width", width)
-			    .attr("height", height)
-			    .attr("preserveAspectRatio", "xMinYMin meet")
-			    .attr("viewBox", `0 0 ${width} ${height}`);
-		
-		let world = svg.append("g");
-		
-		// Add the stripe pattern to the SVG
-		const defs = svg.append("defs");
-		
-		defs.append("pattern")
-		    .attr("id", "stripe")
-		    .attr("patternUnits", "userSpaceOnUse")
-		    .attr("width", 8)
-		    .attr("height", 8)
-		    .attr("patternTransform", "rotate(45)")
-		    .append("rect")
-		    .attr("width", 4)
-		    .attr("height", 8)
-		    .attr("transform", "translate(0,0)")
-		    .attr("opacity", 0.5)
-		    .attr("fill", "grey");
-		
-		fetch("data/story1/choropleth.json") 
+
+	let path = d3.geoPath().projection(projection);
+	
+	// Define color scale
+	const colorScale = d3.scaleThreshold()
+		.domain([100000, 200000, 500000, 1000000, 1500000]) 
+		.range(d3.schemeOranges[6]);
+	
+	let svg = d3.select("#choropleth")
+		    .append("svg")
+		    .attr("width", width)
+		    .attr("height", height)
+		    .attr("preserveAspectRatio", "xMinYMin meet")
+		    .attr("viewBox", `0 0 ${width} ${height}`);
+	
+	let world = svg.append("g");
+	
+	// Add the stripe pattern to the SVG
+	const defs = svg.append("defs");
+	
+	defs.append("pattern")
+	    .attr("id", "stripe")
+	    .attr("patternUnits", "userSpaceOnUse")
+	    .attr("width", 8)
+	    .attr("height", 8)
+	    .attr("patternTransform", "rotate(45)")
+	    .append("rect")
+	    .attr("width", 4)
+	    .attr("height", 8)
+	    .attr("transform", "translate(0,0)")
+	    .attr("opacity", 0.5)
+	    .attr("fill", "grey");
+
+	fetch("data/story1/choropleth.json") 
 		    .then(response => response.json())
 		    .then(data => {
 		        const data_features = topojson.feature(data, data.objects.europe).features;
@@ -106,9 +80,7 @@ const Choropleth = {
 			    
 		        world.selectAll(".states")
 		            .data(data_features)
-		            .enter().append("path")
-			    .attr("data-name", function(d) { return d.properties.name }) 
-				
+		            .enter().append("path") 
 			    // add a class, styling and mouseover/mouseleave
 			    .attr("d", path)
 			    .style("stroke", "black")
@@ -116,20 +88,13 @@ const Choropleth = {
 			    .attr("id", function(d) { return d.id })
 			    .style("opacity", 1)
 			    .style("stroke-width", "0.75px")
-		            .style("fill", function(d) {
-		                // Get data value
-		                var value = d.properties.abundance[yearIndex];
-		                //return mapColour(c(value));
-			        return value != 0 ? colorScale(value) : "url(#stripe)";
-		            })
-			    .on("mouseover", mouseOver)
 			    .on("mouseleave", mouseLeave);
 		    })
 		    .catch(error => {
 		        console.error("Error fetching the data:", error);
 		    });
-		
-		// Legend
+
+	   // Legend
 		const x = d3.scaleLinear()
 			.domain([2.6, 75.1])
 			.rangeRound([600, 860]);
@@ -175,22 +140,53 @@ const Choropleth = {
 			});
 		
 		legend.append("text").attr("x", 15).attr("y", 580).text("NEET abundance");
-		}
 		
-		// Slider interaction
-		const slider = document.getElementById("yearSlider");
-		const selectedYear = document.getElementById("selectedYear");
-		
-		slider.addEventListener("input", function() {
-		    const year = parseInt(this.value);
-		    selectedYear.innerHTML = year;
-		    
-		    // Call the update function to update the map based on the selected year
-		    updateMap(year-2009);
-		});
+		// Update the map initially with data for the year 2009
+	        this.updateMap(0); 
+    	},
 
-		updateMap(0); // Update the map initially with data for the year 2009
-	},
+    	updateMap: function(yearIndex) {
+	    mouseOver = function(event, d) {
+					d3.selectAll(".Country")
+						.transition()
+						.duration(200)
+						.style("opacity", .3)
+						.style("stroke", "black")
+						.style("stroke-width", "0.75px");
+					d3.select(this)
+						.transition()
+						.duration(200)
+						.style("opacity", 1)
+						.style("stroke-width", "2px");
+					// Create the tooltip if it doesn't exist
+				            if (!tooltip) {
+				                tooltip = d3.select("body").append("div")
+				                    .attr("class", "tooltip")
+				                    .style("opacity", 0);
+				            }
+					tooltip.html(d.properties.name + ' &#40;' + d.properties.abbreviation + '&#41;: ' + d.properties.abundance1000[yearIndex] + ' k NEETs')
+						.style("left", (event.pageX + 15) + "px")
+						.style("top", (event.pageY - 28) + "px")
+						.transition().duration(400)
+						.style("opacity", 1);
+				}
+
+		world.selectAll(".states").each(function(d) {
+			d3.select(this)
+			  .style("fill", function() {
+				// Get data value
+		                var value = d.properties.abundance[yearIndex];
+		                //return mapColour(c(value));
+			        return value != 0 ? colorScale(value) : "url(#stripe)";
+            		  })
+			  .on("mouseover", mouseOver);
+		})
+        },
+
+	initialize: function() {
+	    // Call the map creation function during initialization
+	    this.createMap();
+    	},
 
 	destroy: function() {
 	    // Clean up existing map elements
@@ -202,5 +198,17 @@ const Choropleth = {
 	    delete window.Choropleth;
 	}
 }
+
+// Slider interaction
+const slider = document.getElementById("yearSlider");
+const selectedYear = document.getElementById("selectedYear");
+
+slider.addEventListener("input", function() {
+    const year = parseInt(this.value);
+    selectedYear.innerHTML = year;
+    
+    // Call the update function to update the map based on the selected year
+    Choropleth.updateMap(year - 2009);
+});
 
 Choropleth.initialize();
