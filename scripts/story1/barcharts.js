e2f5706a3c2e9051a958cbc4fbf3008a3902a93c
VittorioBartolomeoSecondin <73950586+VittorioBartolomeoSecondin@users.noreply.github.com>
2024-01-03 22:32:42 +0100

    Update barcharts.js

--- scripts/story1/barcharts.js
+++ scripts/story1/barcharts.js
@@ -1,7 +1,7 @@
 const Barcharts = {
     initialize: function(selectedDataset) {
-        const margin = {top: 20, right: 40, bottom: 70, left: 80};
-        const width = 365 - margin.left - margin.right;
+        const margin = {top: 20, right: 40, bottom: 70, left: 50};
+        const width = 250 - margin.left - margin.right;
         const height = 275 - margin.top - margin.bottom;
         const orangeColors = d3.schemeOranges[6];
         const colorDictionary = {
