8d9cf5a8f83f2071f34ef4f4e1b3ed22ae683dd1
christiandagnino <148390006+christiandagnino@users.noreply.github.com>
2024-01-03 12:19:58 +0100

    Update pyramid.js

--- scripts/story1/pyramid.js
+++ scripts/story1/pyramid.js
@@ -1,3 +1,19 @@
- d3.csv(selectedDataset).then(function(data) {
+// set the dimensions and margins of the graph
+var margin = { top: 60, right: 70, bottom: 70, left: 100 },
+	           width = 1435 - margin.left - margin.right,
+	           height = 650 - margin.top - margin.bottom;
+
+// Create a tooltip
+const tooltip = d3.select("#pyramid")
+                  .append("section")
+                    .attr("id", "pyramid_tooltip")
+                  .style("opacity", 0)
+                  .style("background-color", "lightgray")
+                  .style("border", "2px solid black")
+                    .attr("class", "tooltip");
+
+var pyramid_svg;
+
+d3.csv("/data/story1/pyramids/pyramid2009.csv).then(function(data) {
    
- });
+});
