a140ef3aa6b9f37dc0621eee73e6120158eac726
christiandagnino <148390006+christiandagnino@users.noreply.github.com>
2024-01-03 12:44:55 +0100

    Update pyramid.js

--- scripts/story1/pyramid.js
+++ scripts/story1/pyramid.js
@@ -13,59 +13,56 @@ const tooltip = d3.select("#pyramid")
                     .attr("class", "tooltip");
 
 d3.csv("/data/story1/pyramids/pyramid2009.csv").then(function(data) {
-		
-	chart = {
 
-		const svg = d3.create("svg")
-			.attr("viewBox", [0, 0, width, height])
-			.attr("font-family", "sans-serif")
-			.attr("font-size", 10);
+	const svg = d3.create("svg")
+		.attr("viewBox", [0, 0, width, height])
+		.attr("font-family", "sans-serif")
+		.attr("font-size", 10);
+
+	svg.append("g")
+		.selectAll("rect")
+		.data(data)
+		.join("rect")
+		.attr("fill", d => d3.schemeSet1[d.sex === "M" ? 1 : 0])
+		.attr("x", d => d.sex === "M" ? xM(d.percentage) : xF(0))
+		.attr("y", d => y(d.name))
+		.attr("width", d => d.sex === "M" ? xM(0) - xM(d.percentage) : xF(d.percentage) - xF(0))
+		.attr("height", y.bandwidth());
 	
-		svg.append("g")
-			.selectAll("rect")
-			.data(data)
-			.join("rect")
-			.attr("fill", d => d3.schemeSet1[d.sex === "M" ? 1 : 0])
-			.attr("x", d => d.sex === "M" ? xM(d.percentage) : xF(0))
-			.attr("y", d => y(d.name))
-			.attr("width", d => d.sex === "M" ? xM(0) - xM(d.percentage) : xF(d.percentage) - xF(0))
-			.attr("height", y.bandwidth());
-		
-		svg.append("g")
-			.attr("fill", "white")
-			.selectAll("text")
-			.data(data)
-			.join("text")
-			.attr("text-anchor", d => d.sex === "M" ? "start" : "end")
-			.attr("x", d => d.sex === "M" ? xM(d.percentage) + 4 : xF(d.percentage) - 4)
-			.attr("y", d => y(d.name) + y.bandwidth() / 2)
-			.attr("dy", "0.35em")
-			.text(d => d.value.toLocaleString());
-		
-		svg.append("text")
-			.attr("text-anchor", "end")
-			.attr("fill", "white")
-			.attr("dy", "0.35em")
-			.attr("x", xM(0) - 4)
-			.attr("y", y(data[0].name) + y.bandwidth() / 2)
-			.text("Male");
-		
-		svg.append("text")
-			.attr("text-anchor", "start")
-			.attr("fill", "white")
-			.attr("dy", "0.35em")
-			.attr("x", xF(0) + 24)
-			.attr("y", y(data[0].name) + y.bandwidth() / 2)
-			.text("Female");
+	svg.append("g")
+		.attr("fill", "white")
+		.selectAll("text")
+		.data(data)
+		.join("text")
+		.attr("text-anchor", d => d.sex === "M" ? "start" : "end")
+		.attr("x", d => d.sex === "M" ? xM(d.percentage) + 4 : xF(d.percentage) - 4)
+		.attr("y", d => y(d.name) + y.bandwidth() / 2)
+		.attr("dy", "0.35em")
+		.text(d => d.value.toLocaleString());
+	
+	svg.append("text")
+		.attr("text-anchor", "end")
+		.attr("fill", "white")
+		.attr("dy", "0.35em")
+		.attr("x", xM(0) - 4)
+		.attr("y", y(data[0].name) + y.bandwidth() / 2)
+		.text("Male");
+	
+	svg.append("text")
+		.attr("text-anchor", "start")
+		.attr("fill", "white")
+		.attr("dy", "0.35em")
+		.attr("x", xF(0) + 24)
+		.attr("y", y(data[0].name) + y.bandwidth() / 2)
+		.text("Female");
 
-		svg.append("g")
-			.call(xAxis);
+	svg.append("g")
+		.call(xAxis);
 
-		svg.append("g")
-			.call(yAxis);
-		
-		return svg.node();
-	}
+	svg.append("g")
+		.call(yAxis);
+	
+	// return svg.node();
        
 	xM = d3.scaleLinear()
 		.domain([0, d3.max(data, d => d.percentage)])
