2299b2da428438265bec63daf4378eb52af86222
Alessandro Longo <148397539+Ale8599@users.noreply.github.com>
2024-01-03 10:58:42 +0100

    Update linechart.js

--- scripts/story1/linechart.js
+++ scripts/story1/linechart.js
@@ -1,4 +1,13 @@
+// set the dimensions and margins of the graph
 var margin = { top: 60, right: 70, bottom: 70, left: 100 },
 	           width = 1435 - margin.left - margin.right,
 	           height = 650 - margin.top - margin.bottom;
 
+// Create a tooltip
+const tooltip = d3.select("#linechart_1")
+                  .append("section")
+                    .attr("id", "linechart_tooltip")
+                  .style("opacity", 0)
+                  .style("background-color", "lightgray")
+                  .style("border", "2px solid black")
+                    .attr("class", "tooltip");
