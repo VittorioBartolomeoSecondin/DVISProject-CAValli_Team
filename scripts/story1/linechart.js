681e2405d8cf1785aabb00a3f77928a7b9d0a4cb
Alessandro Longo <148397539+Ale8599@users.noreply.github.com>
2024-01-03 13:58:34 +0100

    Update linechart.js

--- scripts/story1/linechart.js
+++ scripts/story1/linechart.js
@@ -1,18 +1,18 @@
-// Function to load CSV data and create line chart
-function drawLineChart(selectedCountries) {
-    // Set up the SVG dimensions
-    var margin = { top: 60, right: 70, bottom: 70, left: 100 },
-	           width = 1435 - margin.left - margin.right,
-	           height = 650 - margin.top - margin.bottom;
-
-    // Create a tooltip
-    const tooltip = d3.select("#linechart_1")
+ // Create a tooltip
+const tooltip = d3.select("#linechart_1")
 		  .append("section")
 		    .attr("id", "linechart_tooltip")
 		  .style("opacity", 0)
 		  .style("background-color", "lightgray")
 		  .style("border", "2px solid black")
 		    .attr("class", "tooltip");
+
+// Function to load CSV data and create line chart
+function drawLineChart(selectedCountries) {
+    // Set up the SVG dimensions
+    var margin = { top: 60, right: 70, bottom: 70, left: 100 },
+	           width = 1435 - margin.left - margin.right,
+	           height = 650 - margin.top - margin.bottom;
 
     // Append an SVG element to the specified div
     var svg = d3.select("#linechart_1")
