3c38479481b40dc82cce942d1ad523ff674fe057
VittorioBartolomeoSecondin <73950586+VittorioBartolomeoSecondin@users.noreply.github.com>
2024-01-04 21:14:43 +0100

    Update pyramid.js

--- scripts/story1/pyramid.js
+++ scripts/story1/pyramid.js
@@ -1,5 +1,5 @@
 // set the dimensions and margins of the graph
-var margin = { top: 10, right: 50, bottom: 50, left: 50 },
+var margin = { top: 10, right: 50, bottom: 50, left: 30 },
 	           width = 850 - margin.left - margin.right,
 	           height = 590 - margin.top - margin.bottom;
 
