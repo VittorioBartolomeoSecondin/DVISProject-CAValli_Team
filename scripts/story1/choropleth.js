ec733f32b40858bc399ad002f3b9d46de7443b5a
VittorioBartolomeoSecondin <73950586+VittorioBartolomeoSecondin@users.noreply.github.com>
2024-01-04 16:33:13 +0100

    Update choropleth.js

--- scripts/story1/choropleth.js
+++ scripts/story1/choropleth.js
@@ -82,7 +82,7 @@ const Choropleth = {
 			    .rangeRound([600, 860]);
 			
 		const legend = svg.append("g")
-				  .attr("id", "choropleth_legend")
+				  .attr("id", "choropleth-legend")
 				  .attr("transform", `translate(${legendX}, ${legendY})`);
 			
 		const legend_entry = legend.selectAll("g.legend")
