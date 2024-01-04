926b35aba9b0d420bf94f6047ca4be4f461bfdd0
VittorioBartolomeoSecondin <73950586+VittorioBartolomeoSecondin@users.noreply.github.com>
2024-01-04 17:38:32 +0100

    Update pyramid.js

--- scripts/story1/pyramid.js
+++ scripts/story1/pyramid.js
@@ -1,6 +1,6 @@
 // set the dimensions and margins of the graph
 var margin = { top: 50, right: 50, bottom: 50, left: 50 },
-	           width = 2000 - margin.left - margin.right,
+	           width = 500 - margin.left - margin.right,
 	           height = 800 - margin.top - margin.bottom;
 
 d3.csv("data/story1/pyramids/pyramid2009.csv").then(function(data) {
