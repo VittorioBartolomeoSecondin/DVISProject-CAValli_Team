b90f337eec2b0f8f3e3a4474957d7a308a738f39
VittorioBartolomeoSecondin <73950586+VittorioBartolomeoSecondin@users.noreply.github.com>
2024-01-04 12:25:16 +0100

    Update linechart.js

--- scripts/story1/linechart.js
+++ scripts/story1/linechart.js
@@ -18,7 +18,7 @@ function drawLineChart(selectedCountries) {
 
     // Set up the SVG dimensions
     var margin = { top: 60, right: 70, bottom: 70, left: 100 },
-	    width = 800 - margin.left - margin.right,
+	    width = 750 - margin.left - margin.right,
 	    height = 650 - margin.top - margin.bottom;
 	
     // Append an SVG element to the specified div
