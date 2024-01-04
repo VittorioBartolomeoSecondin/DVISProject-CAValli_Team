206f21cad03868ee24a7c5b370eca16e4076c572
VittorioBartolomeoSecondin <73950586+VittorioBartolomeoSecondin@users.noreply.github.com>
2024-01-04 18:25:30 +0100

    Update pyramid.js

--- scripts/story1/pyramid.js
+++ scripts/story1/pyramid.js
@@ -50,7 +50,6 @@ d3.csv("data/story1/pyramids/pyramid2009.csv").then(function(data) {
 		.data(data)
 		.enter()
 		.append("rect")
-		.attr("transform", `translate(-10,0)`)
 		.attr("x", d => d.sex === "M" ? xM(+d.percentage) : xF(0))
 		// .attr("y", d => y(d.name))
 		.attr("y", d => y(d.abbreviation))
