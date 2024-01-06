1084e7e599b1c5bbec7e0e000801232ddce8602b
VittorioBartolomeoSecondin <73950586+VittorioBartolomeoSecondin@users.noreply.github.com>
2024-01-06 11:58:14 +0100

    Update donuts.js

--- scripts/story2/donuts.js
+++ scripts/story2/donuts.js
@@ -33,11 +33,6 @@ const arc = d3.arc()
   .innerRadius(radius * 0.5)         // This is the size of the donut hole
   .outerRadius(radius * 0.8)
 
-// Another arc that won't be drawn. Just for labels positioning
-const outerArc = d3.arc()
-  .innerRadius(radius * 0.9)
-  .outerRadius(radius * 0.9)
-
 // Build the pie chart: Basically, each part of the pie is a path that we build using the arc function.
 svg
   .selectAll('allSlices')
@@ -48,20 +43,3 @@ svg
   .attr("stroke", "white")
   .style("stroke-width", "2px")
   .style("opacity", 0.7)
-
-// Add the polylines between chart and labels:
-svg
-  .selectAll('allPolylines')
-  .data(data_ready)
-  .join('polyline')
-    .attr("stroke", "black")
-    .style("fill", "none")
-    .attr("stroke-width", 1)
-    .attr('points', function(d) {
-      const posA = arc.centroid(d) // line insertion in the slice
-      const posB = outerArc.centroid(d) // line break: we use the other arc generator that has been built only for that
-      const posC = outerArc.centroid(d); // Label position = almost the same as posB
-      const midangle = d.startAngle + (d.endAngle - d.startAngle) / 2 // we need the angle to see if the X position will be at the extreme right or extreme left
-      posC[0] = radius * 0.95 * (midangle < Math.PI ? 1 : -1); // multiply by 1 or -1 to put it on the right or on the left
-      return [posA, posB, posC]
-    })
