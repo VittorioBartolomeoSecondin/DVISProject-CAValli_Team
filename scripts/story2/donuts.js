dbf4a17e186f2eb653d8edacc5d924722338188e
VittorioBartolomeoSecondin <73950586+VittorioBartolomeoSecondin@users.noreply.github.com>
2024-01-06 11:59:59 +0100

    Update donuts.js

--- scripts/story2/donuts.js
+++ scripts/story2/donuts.js
@@ -1,6 +1,6 @@
 // set the dimensions and margins of the graph
-const width = 450,
-    height = 450,
+const width = 350,
+    height = 350,
     margin = 40;
 
 // The radius of the pieplot is half the width or half the height (smallest one). I subtract a bit of margin.
