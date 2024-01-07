257f6dc58381fb8e421491daca9459c928bb538f
VittorioBartolomeoSecondin <73950586+VittorioBartolomeoSecondin@users.noreply.github.com>
2024-01-07 16:55:19 +0100

    Update barcharts.js

--- scripts/story1/barcharts.js
+++ scripts/story1/barcharts.js
@@ -92,7 +92,9 @@ const Barcharts = {
 						.attr("class", "tooltip")
 						.style("opacity", 0);
 				        }
-					tooltip.html(`<span style="color:${colorDictionary[key]}"><b>${d.name}</b></span>: ${Math.floor(d.abundance)}k NEETs`)
+					tooltip.html(`<span style="color:${colorDictionary[key]}; text-shadow: -1px -1px 0 #000, 
+     							1px -1px 0 #000, -1px 1px 0 #000, 1px 1px 0 #000;"><b>${d.name}</b></span>:
+	    						${Math.floor(d.abundance)}k NEETs`)
 					    .style("left", (event.pageX + 15) + "px")
 					    .style("top", (event.pageY - 28) + "px")
 					    .transition().duration(400)
