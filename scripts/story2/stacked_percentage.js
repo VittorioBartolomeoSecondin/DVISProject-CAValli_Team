50a097309bdb8b306fc2dcfa07e9df55bbd6d85c
VittorioBartolomeoSecondin <73950586+VittorioBartolomeoSecondin@users.noreply.github.com>
2024-01-08 16:58:14 +0100

    Update stacked_percentage.js

--- scripts/story2/stacked_percentage.js
+++ scripts/story2/stacked_percentage.js
@@ -161,7 +161,7 @@ function updateStackedPChart(selectedValue) {
            	// Customize the tooltip content
            	tooltip.html(`Education level: <b>${subgroupName.charAt(0).toUpperCase() + 
 				subgroupName.slice(1)}</b><br>Percentage: ${(+subgroupValue).toFixed(1)}%<br>
-	                        Absolute value: ${(+subgroupOriginalValue).toFixed(0)} k persons`)
+	                        Absolute value: ${(+subgroupOriginalValue).toFixed(0)}k persons`)
 			.style("left", (event.pageX + 10) + "px")
                   	.style("top", (event.pageY - 20) + "px");
 		   
