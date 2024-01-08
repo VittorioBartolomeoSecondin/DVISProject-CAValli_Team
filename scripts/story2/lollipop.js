96ea054ee92123c6df1c88aea4b5c3fd2879d876
VittorioBartolomeoSecondin <73950586+VittorioBartolomeoSecondin@users.noreply.github.com>
2024-01-08 21:06:01 +0100

    Update lollipop.js

--- scripts/story2/lollipop.js
+++ scripts/story2/lollipop.js
@@ -2,7 +2,7 @@ function updateLollipopChart(selectedValue) {
 	
 	// Set up the SVG dimensions
 	var margin = { top: 30, right: 70, bottom: 90, left: 100 },
-		      width = 900 - margin.left - margin.right,
+		      width = 800 - margin.left - margin.right,
 		      height = 625 - margin.top - margin.bottom;
 	
 	// append the svg object to the body of the page
