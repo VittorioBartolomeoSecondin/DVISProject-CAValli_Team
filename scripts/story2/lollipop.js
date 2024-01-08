049d375503085a2155019429a546b830e32954c4
VittorioBartolomeoSecondin <73950586+VittorioBartolomeoSecondin@users.noreply.github.com>
2024-01-08 21:26:05 +0100

    Update lollipop.js

--- scripts/story2/lollipop.js
+++ scripts/story2/lollipop.js
@@ -1,6 +1,6 @@
 function updateLollipopChart(selectedValue) {
 	
-	/*// Set up the SVG dimensions
+	// Set up the SVG dimensions
 	var margin = { top: 30, right: 70, bottom: 90, left: 100 },
 		      width = 750 - margin.left - margin.right,
 		      height = 625 - margin.top - margin.bottom;
@@ -20,48 +20,47 @@ function updateLollipopChart(selectedValue) {
 	  // Treat the first row separately
     	  const firstRow = data.shift(); // Remove and save the first row
 	  
-	  // X axis
-	  const x = d3.scaleBand()
+	  // Add X axis
+	  const x = d3.scaleLinear()
+	    .domain([0, 55])
+	    .range([height, 0]);
+	  svg.append("g")
+	    .call(d3.axisLeft(x).tickFormat((d) => (d === 0 ? d : d + "%")));
+
+	   // Y axis
+	  const y = d3.scaleBand()
 	    .range([ 0, width ])
 	    .domain(data.map(function(d) { return d.name; }))
 	    .padding(1);
 	  svg.append("g")
 	    .attr("transform", `translate(0, ${height})`)
-	    .call(d3.axisBottom(x))
+	    .call(d3.axisBottom(y))
 	    .selectAll("text")
-	      .attr("transform", "translate(-10,0)rotate(-45)")
 	      .style("text-anchor", "end");
-	  
-	  // Add Y axis
-	  const y = d3.scaleLinear()
-	    .domain([0, 55])
-	    .range([height, 0]);
-	  svg.append("g")
-	    .call(d3.axisLeft(y).tickFormat((d) => (d === 0 ? d : d + "%")));
 
-	  // Horizontal line
+	  // Vertical line
 	  svg.append("line")
-	    .attr("x1", 0)
-  	    .attr("x2", width)
-	    .attr("y1", y(firstRow.abundance))
-	    .attr("y2", y(firstRow.abundance))
+	    .attr("x1", x(firstRow.abundance)) // Change the x1 to an x-coordinate value
+	    .attr("x2", x(firstRow.abundance)) // Change the x2 to the same x-coordinate value
+	    .attr("y1", 0) // Start from the top of the chart
+	    .attr("y2", height) // Extend to the bottom of the chart
 	    .attr("stroke", "red")
 	    .attr("stroke-dasharray", "5,5");
 
-	  // Text label horizontal line
+	  // Text label vertical line
 	  svg.append("text")
-	   .attr("x", width + 5) // Adjust the x-coordinate for positioning
-	   .attr("y", y(firstRow.abundance))
-	   .attr("dy", "0.35em")
+	   .attr("x", x(firstRow.abundance)) // Adjust the x-coordinate for positioning
+	   .attr("y", height + 5y(firstRow.abundance))
+	   .attr("dx", "0.35em")
 	   .style("font-size", "12px")
 	   .text("Europe")
 	   .attr("fill", "red");
 		
-	  // Text label horizontal line
+	  // Text label vertical line
 	  svg.append("text")
-	   .attr("x", width - 40)
-	   .attr("y", y(firstRow.abundance) - 12)
-	   .attr("dy", "0.35em")
+	   .attr("x", x(firstRow.abundance) - 12)
+	   .attr("y", height - 40)
+	   .attr("dx", "0.35em")
 	   .style("font-size", "12px")
 	   .text(firstRow.abundance + "%")
 	   .attr("fill", "red");
@@ -71,18 +70,18 @@ function updateLollipopChart(selectedValue) {
 	    .data(data)
 	    .enter()
 	    .append("line")
-	      .attr("x1", function(d) { return x(d.name); })
-	      .attr("x2", function(d) { return x(d.name); })
-	      .attr("y1", function(d) { return y(d.abundance); })
-	      .attr("y2", y(0))
+	      .attr("x1", function(d) { return x(d.abundance); })
+	      .attr("x2", x(0))
+	      .attr("y1", function(d) { return x(d.name); })
+	      .attr("y2", function(d) { return x(d.name); })
 	      .attr("stroke", "grey")
 	  
 	  // Circles
 	  svg.selectAll("mycircle")
 	    .data(data)
 	    .join("circle")
-	      .attr("cx", function(d) { return x(d.name); })
-	      .attr("cy", function(d) { return y(d.abundance); })
+	      .attr("cx", function(d) { return x(d.abundance); })
+	      .attr("cy", function(d) { return y(d.name); })
 	      .attr("r", "8")
 	      .style("fill", "#69b3a2")
 	      .attr("stroke", "black")
@@ -90,65 +89,7 @@ function updateLollipopChart(selectedValue) {
 	      .on("mouseover", LollipopChartMouseOver)
 	      .on("mouseout", LollipopChartMouseOut);
 	})
-}*/
-
-// Horizontal line (now vertical)
-  svg.append("line")
-    .attr("x1", y(firstRow.abundance)) // x1 and x2 are swapped
-    .attr("x2", y(firstRow.abundance)) // x1 and x2 are swapped
-    .attr("y1", 0) // y1 and y2 are swapped
-    .attr("y2", height) // y1 and y2 are swapped
-    .attr("stroke", "red")
-    .attr("stroke-dasharray", "5,5")
-    .attr("transform", "translate(" + margin.left + ",0)"); // Adjust for horizontal orientation
-  
-  // Text label horizontal line (now vertical)
-  svg.append("text")
-    .attr("x", y(firstRow.abundance) + 5) // Adjust the x-coordinate for positioning
-    .attr("y", height + margin.top + 5) // Adjust for horizontal orientation
-    .attr("dy", "0.35em")
-    .style("font-size", "12px")
-    .text("Europe")
-    .attr("fill", "red")
-    .attr("transform", "translate(" + margin.left + ",0)"); // Adjust for horizontal orientation
-  
-  // Text label horizontal line (now vertical)
-  svg.append("text")
-    .attr("x", y(firstRow.abundance) + 12) // Adjust the x-coordinate for positioning
-    .attr("y", height - 40) // Adjust for horizontal orientation
-    .attr("dy", "0.35em")
-    .style("font-size", "12px")
-    .text(firstRow.abundance + "%")
-    .attr("fill", "red")
-    .attr("transform", "translate(" + margin.left + ",0)"); // Adjust for horizontal orientation
-  
-  // Lines (now horizontal)
-  svg.selectAll("myline")
-    .data(data)
-    .enter()
-    .append("line")
-      .attr("x1", 0) // x1 and x2 are swapped
-      .attr("x2", function(d) { return y(d.abundance); }) // x1 and x2 are swapped
-      .attr("y1", function(d) { return x(d.name); }) // y1 and y2 are swapped
-      .attr("y2", function(d) { return x(d.name); }) // y1 and y2 are swapped
-      .attr("stroke", "grey")
-      .attr("transform", "translate(" + margin.left + ",0)"); // Adjust for horizontal orientation
-  
-  // Circles (now horizontal)
-  svg.selectAll("mycircle")
-    .data(data)
-    .join("circle")
-      .attr("cx", function(d) { return y(d.abundance); }) // cx and cy are swapped
-      .attr("cy", function(d) { return x(d.name); }) // cx and cy are swapped
-      .attr("r", "8")
-      .style("fill", "#69b3a2")
-      .attr("stroke", "black")
-      .attr("stroke-width", 1)
-      .on("mouseover", LollipopChartMouseOver)
-      .on("mouseout", LollipopChartMouseOut)
-      .attr("transform", "translate(" + margin.left + ",0)"); // Adjust for horizontal orientation
 }
-
 
 function LollipopChartMouseOver(event, d) {
     d3.select(this).attr("stroke-width", 2);
