95ab7ce23f8d7075d62b592221ee3d8d0a78445d
VittorioBartolomeoSecondin <73950586+VittorioBartolomeoSecondin@users.noreply.github.com>
2024-01-08 21:36:02 +0100

    Update lollipop.js

--- scripts/story2/lollipop.js
+++ scripts/story2/lollipop.js
@@ -20,23 +20,24 @@ function updateLollipopChart(selectedValue) {
 	  // Treat the first row separately
     	  const firstRow = data.shift(); // Remove and save the first row
 	  
-	  // Y axis
-	  const y = d3.scaleBand()
-	    .range([0,  height])
-	    .domain(data.map(function(d) { return d.name; }))
-	    .padding(1);
-	  svg.append("g")
-	    .attr("transform", `translate(0, ${height})`)
-	    .call(d3.axisBottom(y))
-	    .selectAll("text")
-	      .style("text-anchor", "end");
-	  
-	  // Add X axis
-	  const x = d3.scaleLinear()
-	    .domain([0, 55])
-	    .range([0, width]);
-	  svg.append("g")
-	    .call(d3.axisLeft(x).tickFormat((d) => (d === 0 ? d : d + "%")));
+	  // Y axis as the horizontal axis
+	const y = d3.scaleBand() // Use scaleBand for the y-axis
+	  .range([0, height]) // Use height for the range
+	  .domain(data.map(function(d) { return d.name; }))
+	  .padding(1);
+	svg.append("g")
+	  .call(d3.axisLeft(y))
+	  .selectAll("text")
+	    .attr("transform", "translate(-10,0)rotate(-45)")
+	    .style("text-anchor", "end");
+	
+	// X axis as the vertical axis
+	const x = d3.scaleLinear() // Use scaleLinear for the x-axis
+	  .domain([0, 55]) // Use 0 to 55 for the domain
+	  .range([0, width]); // Use width for the range
+	svg.append("g")
+	  .attr("transform", `translate(0, ${height})`)
+	  .call(d3.axisBottom(x).tickFormat((d) => (d === 0 ? d : d + "%")));
 
 	  // Vertical line
 	  svg.append("line")
