57e8730f2660f88ab86e1c66112acb701f295d36
Alessandro Longo <148397539+Ale8599@users.noreply.github.com>
2024-01-08 17:10:03 +0100

    Update streamgraph.js

--- scripts/story2/streamgraph.js
+++ scripts/story2/streamgraph.js
@@ -41,7 +41,7 @@ d3.csv("data/story2/streamgraph/streamgraph_final_vero.csv").then( function(data
 
   // Add Y axis
   const y = d3.scaleLinear()
-    .domain([-600000, 600000])
+    .domain([-5000000, 5000000])
     .range([ height, 0]);
   svg.append("g")
     .call(d3.axisLeft(y));
