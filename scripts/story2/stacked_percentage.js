8b5ba0597e0f214792bcf8b7632b0b397b45a538
VittorioBartolomeoSecondin <73950586+VittorioBartolomeoSecondin@users.noreply.github.com>
2024-01-08 16:54:51 +0100

    Update stacked_percentage.js

--- scripts/story2/stacked_percentage.js
+++ scripts/story2/stacked_percentage.js
@@ -184,7 +184,7 @@ function updateStackedPChart(selectedValue) {
 	
 	    // Animation
 	    .transition()
-	    .duration(1500)
+	    .duration(2000)
 	    .attr("width", d => x(d[1]) - x(d[0])) // Transition to the actual width
 	    .delay((d, i) => i * 100); // Add delay for staggered animation
 	  
