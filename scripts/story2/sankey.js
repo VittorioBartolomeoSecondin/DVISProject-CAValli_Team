b11f305cebe6d6f30712558cf4ef9fbcc726b2b1
Alessandro Longo <148397539+Ale8599@users.noreply.github.com>
2024-01-09 17:55:53 +0100

    Update sankey.js

--- scripts/story2/sankey.js
+++ scripts/story2/sankey.js
@@ -119,37 +119,6 @@ d3.csv("data/story2/sankey/sankey_EU.csv").then(function(data) {
     d3.select(this).attr("font-weight", "normal");
    });
 
-   var showing_connections = false; 
-   var prevClickedNode = null; 
-	
-   // Add hover effects to nodes
-   node.on("click", function(event, d) {
-      // Toggle the state when a node is clicked
-     showing_connections = !showing_connections;
-	
-     // Check if it's the same node as previously clicked
-     if (prevClickedNode && prevClickedNode !== d) {
-	 link.each(function(linkData) {
-	    if (linkData.target.name === prevClickedNode.name) {
-		 d3.select(document.getElementById(linkData.source.name + "->" + prevClickedNode.name))
-		   .style("stroke-opacity", 0.2)
-		   .attr("stroke-width", function(d) { return d.width; });
-	     }
-          });
-      }
-	
-     // Update the link styles based on the state
-     link.each(function(linkData) {
-        if (linkData.target.name === d.name) {
-	    d3.select(document.getElementById(linkData.source.name + "->" + d.name))
-	      .style("stroke-opacity", showing_connections ? 0.5 : 0.2)
-	      .attr("stroke-width", function(d) { return showing_connections ? Math.max(4, d.width) : d.width; });
-	 }
-     });
-	
-     // Keep track of the clicked node
-     prevClickedNode = d;
-  });
   
    // Add hover effects to links
    link.on("mouseover", function () {
