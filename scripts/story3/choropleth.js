884ba2aebe6a66ac0a7c84d788faf38897910e66
VittorioBartolomeoSecondin <73950586+VittorioBartolomeoSecondin@users.noreply.github.com>
2024-01-10 18:05:53 +0100

    Update choropleth.js

--- scripts/story3/choropleth.js
+++ scripts/story3/choropleth.js
@@ -24,7 +24,7 @@ const ItalianChoropleth = {
 				  .center([0, 41])
 				  .rotate([347, 0])
 			          .parallels([35, 45])
-				  .scale(2500)
+				  .scale(2400)
 				  .translate([width / 2, height / 2]);
 	
 	       const self = this; // saving a reference to the Choropleth object
