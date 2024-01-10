de942c12a6804969ad6ae4f65c401562deb5f57b
VittorioBartolomeoSecondin <73950586+VittorioBartolomeoSecondin@users.noreply.github.com>
2024-01-10 13:55:43 +0100

    Update dumbbell.js

--- scripts/story3/dumbbell.js
+++ scripts/story3/dumbbell.js
@@ -1,6 +1,6 @@
 // set the dimensions and margins of the graph
-const margin = {top: 10, right: 30, bottom: 30, left: 30},
-    width = 460 - margin.left - margin.right,
+const margin = {top: 10, right: 50, bottom: 30, left: 30},
+    width = 600 - margin.left - margin.right,
     height = 500 - margin.top - margin.bottom;
 
 // append the svg object to the body of the page
