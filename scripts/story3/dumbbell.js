fa3f060ef76580d564960c8977fc1008c397c993
VittorioBartolomeoSecondin <73950586+VittorioBartolomeoSecondin@users.noreply.github.com>
2024-01-10 17:26:24 +0100

    Update dumbbell.js

--- scripts/story3/dumbbell.js
+++ scripts/story3/dumbbell.js
@@ -12,7 +12,7 @@ const svg = d3.select("#dumbbell")
     .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
 // Parse the Data
-d3.csv("data/story3/dumbbell/dumbbell.csv").then( function(data) {
+d3.csv("data/story3/dumbbell.csv").then( function(data) {
 
 
   // Add X axis
