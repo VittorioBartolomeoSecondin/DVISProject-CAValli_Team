80e750a23c22bbac6ddd09bb3eee3b3394b7caba
Alessandro Longo <148397539+Ale8599@users.noreply.github.com>
2024-01-11 11:13:07 +0100

    Update bubblechart.js

--- scripts/story2/bubblechart.js
+++ scripts/story2/bubblechart.js
@@ -35,7 +35,7 @@ d3.csv("data/story2/bubblechart/bubblechart.csv").then( function(data) {
 
   // Add a scale for bubble size
   const z = d3.scaleLinear()
-    .domain([1000, 4000000000])
+    .domain([1000, 40000000])
     .range([5, 50]);
 
   // Add dots
