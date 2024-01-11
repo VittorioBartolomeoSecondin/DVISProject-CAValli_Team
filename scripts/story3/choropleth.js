fb57d539cf9c78dc9bae50442302bd969096ff1c
VittorioBartolomeoSecondin <73950586+VittorioBartolomeoSecondin@users.noreply.github.com>
2024-01-11 19:15:08 +0100

    Update choropleth.js

--- scripts/story3/choropleth.js
+++ scripts/story3/choropleth.js
@@ -17,8 +17,8 @@ const ItalianChoropleth = {
 	       const legendHeight = 300;
 	
 	       // Calculate the position for the legend
-	       const legendX = width - legendWidth - 80; 
-	       const legendY = height / 2 - legendHeight / 2 - 300; 
+	       const legendX = width - legendWidth - 80 - 175; 
+	       const legendY = height / 2 - legendHeight / 2 - 300 + 30; 
 			
 	       let projection = d3.geoAlbers()
 				  .center([0, 41])
