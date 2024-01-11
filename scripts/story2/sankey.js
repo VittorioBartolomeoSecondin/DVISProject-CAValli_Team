5f4777ea6a7706bb8d7d607b732f8c883e4d322e
VittorioBartolomeoSecondin <73950586+VittorioBartolomeoSecondin@users.noreply.github.com>
2024-01-11 18:18:18 +0100

    Update sankey.js

--- scripts/story2/sankey.js
+++ scripts/story2/sankey.js
@@ -7,7 +7,7 @@ function createSankeys() {
 	// Set up the SVG dimensions
 	var margin = { top: 30, right: 90, bottom: 30, left: 90 },
 		width = 650 - margin.left - margin.right,
-		height = 450 - margin.top - margin.bottom;  
+		height = 350 - margin.top - margin.bottom;  
 	
 	for (var i = 1; i <= numSankeys; i++) {
 	    	var containerId = "#sankey" + i;
