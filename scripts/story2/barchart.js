d672187f95b23f906a30a2763bb09d1b6f997f95
Alessandro Longo <148397539+Ale8599@users.noreply.github.com>
2024-01-12 15:59:02 +0100

    Update barchart.js

--- scripts/story2/barchart.js
+++ scripts/story2/barchart.js
@@ -1,3 +1,4 @@
+/*
 // Set up the SVG dimensions
 var margin = { top: 30, right: 20, bottom: 90, left: 100 },
 		        width = 950 - margin.left - margin.right,
@@ -64,4 +65,62 @@ d3.csv("data/story2/barcharts/barchart_AUT.csv").then( function(data) {
       .attr("height", d => height - y(d.value))
       .attr("fill", d => (d.indicator === "Sex") ? "blue" : (d.indicator === "Age range") ? "red" : (d.indicator === "Education") ? "green" : "purple");
       //.attr("fill", "#69b3a2")
+})
+*/
+/ Set up the SVG dimensions
+var margin = { top: 30, right: 70, bottom: 90, left: 100 },
+		        width = 750 - margin.left - margin.right,
+		        height = 650 - margin.top - margin.bottom;
+
+// append the svg object to the body of the page
+const svg = d3.select("#barchart")
+  .append("svg")
+    .attr("id", "barchart_svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform", `translate(${margin.left},${margin.top})`);
+
+// Parse the Data
+d3.csv("data/story2/barcharts/barchart_AUT.csv").then( function(data) {
+
+  // Filter out data points for the relevant year
+  data = data.filter(d => d.year == 2009);
+
+  // Extract unique categories for the x-axis
+  const categories = [...new Set(data.map(d => d.category))];
+	
+  console.log(categories);
+  
+  // X axis
+  const x = d3.scaleBand()
+    .range([0, width])
+    .domain(categories)
+    .padding(0.2);
+  svg.append("g")
+    .attr("transform", `translate(0, ${height})`)
+    .call(d3.axisBottom(x))
+    .selectAll("text")
+      .attr("transform", "rotate(-90)")
+      .style("text-anchor", "end")
+      .attr("dy", "-3.85em");
+  
+  // Add Y axis
+  const y = d3.scaleLinear()
+    .domain([0, 70])
+    .range([height, 0]);
+  svg.append("g")
+    .call(d3.axisLeft(y).tickFormat((d) => (d === 0 ? d : d + "%")));
+  
+  // Bars
+  svg.selectAll("rect")
+    .data(data)
+    .enter()
+    .append("rect")
+      .attr("x", d => x(d.category))
+      .attr("y", d => y(d.value))
+      .attr("width", x.bandwidth())
+      .attr("height", d => height - y(d.value))
+      .attr("fill", "#69b3a2")
+
 })
