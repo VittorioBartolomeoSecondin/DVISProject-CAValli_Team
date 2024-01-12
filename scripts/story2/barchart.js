a533efd11b6b341d56190f72d9df940452db8e51
Alessandro Longo <148397539+Ale8599@users.noreply.github.com>
2024-01-12 13:27:16 +0100

    Update barchart.js

--- scripts/story2/barchart.js
+++ scripts/story2/barchart.js
@@ -1,6 +1,6 @@
 // Set up the SVG dimensions
 var margin = { top: 30, right: 70, bottom: 90, left: 100 },
-		        width = 1200 - margin.left - margin.right,
+		        width = 750 - margin.left - margin.right,
 		        height = 650 - margin.top - margin.bottom;
 
 // append the svg object to the body of the page
@@ -26,7 +26,7 @@ d3.csv("data/story2/barcharts/barchart_AUT.csv").then( function(data) {
   console.log(indicators);
   
   // X axis
-  const x = d3.scaleBand()
+  /*const x = d3.scaleBand()
     .range([0, width])
     .domain(categories)
     .padding(0.2);
@@ -36,7 +36,21 @@ d3.csv("data/story2/barcharts/barchart_AUT.csv").then( function(data) {
     .selectAll("text")
       .attr("transform", "translate(-10,0)rotate(-90)")
       .style("text-anchor", "middle")
-      .attr("dx", "-3.85em");
+      .attr("dx", "-3.85em");*/
+	
+  // X axis
+  const x = d3.scaleBand()
+      .range([0, width])
+      .domain(categories)
+      .padding(0.2);
+
+  const xAxis = svg.append("g")
+      .attr("transform", `translate(0, ${height})`)
+      .call(d3.axisBottom(x))
+      .selectAll("text")
+          .attr("transform", "translate(-10,0)rotate(-90)")
+          .style("text-anchor", "middle")
+          .attr("dx", "-3.85em");
   
   // Add Y axis
   const y = d3.scaleLinear()
@@ -52,6 +66,22 @@ d3.csv("data/story2/barcharts/barchart_AUT.csv").then( function(data) {
     .append("g")
     .attr("class", d => `indicator-group ${d}`)
     .attr("transform", d => `translate(${indicators.indexOf(d) * width / indicators.length}, 0)`);
+  
+  // Move x-axis labels to align with the corresponding bars
+  indicatorGroups.each(function(d) {
+      const group = d3.select(this);
+      const translation = group.attr("transform");
+      xAxis.each(function() {
+          const label = d3.select(this);
+          const labelCategory = label.text();
+          const matchingBar = group.select(`rect[data-category="${labelCategory}"]`);
+          const barX = +matchingBar.attr("x");
+          const barWidth = +matchingBar.attr("width");
+          const barCenterX = barX + barWidth / 2;
+          const labelX = x(labelCategory) + +translation.split("(")[1].split(",")[0] + barCenterX;
+          label.attr("x", labelX);
+      });
+  });
   
   // Bars
   indicatorGroups.selectAll("rect")
