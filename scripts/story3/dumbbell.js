21525a8d6ac3bd4406dbefa31e0199a8717c11cc
VittorioBartolomeoSecondin <73950586+VittorioBartolomeoSecondin@users.noreply.github.com>
2024-01-12 11:24:16 +0100

    Update dumbbell.js

--- scripts/story3/dumbbell.js
+++ scripts/story3/dumbbell.js
@@ -99,7 +99,10 @@ const Dumbbell = {
 	   .attr("dx", "-2.7em")
 	   .attr("dy", "-0.5em")
 	   .style("font-size", "12px")
-	   .html(`Italia<br>2013`)
+	   .html(function () {
+		    return "Italia" +
+		      "<tspan x='" + x(firstRow['2013_perc']) + "' dy='1.2em'>2013</tspan>";
+  	   })
 	   .attr("fill", "#69b3a2");
 
 	  // Text label vertical line
