a3a309fcb886631714c4668b89e5a20a6f5993f9
Alessandro Longo <148397539+Ale8599@users.noreply.github.com>
2024-01-12 12:22:16 +0100

    Update barchart.js

--- scripts/story2/barchart.js
+++ scripts/story2/barchart.js
@@ -19,8 +19,14 @@ d3.csv("data/story2/barcharts/barchart_AUT.csv").then( function(data) {
   data = data.filter(d => d.year === 2009);
 
   // Extract unique categories for the x-axis
-  const categories = [...new Set(data.map(d => d.category))];
-  console.log(categories);
+  // List of subgroups = header of the csv files = soil condition here
+  var subgroups = data.columns.slice(1)
+
+  // List of groups = species here = value of the first column called group -> I show them on the X axis
+  var groups = d3.map(data, function(d){return(d.group)}).keys()
+	
+  console.log(subgroups);
+  console.log(groups);
   
   // X axis
   const x = d3.scaleBand()
@@ -39,7 +45,7 @@ d3.csv("data/story2/barcharts/barchart_AUT.csv").then( function(data) {
     .domain([0, 100])
     .range([height, 0]);
   svg.append("g")
-    .call(d3.axisLeft(y));
+    .call(d3.axisLeft(y).tickFormat((d) => (d === 0 ? d : d + "%")));
   
   // Bars
   svg.selectAll("mybar")
