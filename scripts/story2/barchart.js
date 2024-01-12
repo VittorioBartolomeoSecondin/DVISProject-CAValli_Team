4634075c36be66268feb05572f5fb7edd07be9d4
Alessandro Longo <148397539+Ale8599@users.noreply.github.com>
2024-01-12 13:00:35 +0100

    Update barchart.js

--- scripts/story2/barchart.js
+++ scripts/story2/barchart.js
@@ -40,7 +40,7 @@ d3.csv("data/story2/barcharts/barchart_AUT.csv").then( function(data) {
   
   // Add Y axis
   const y = d3.scaleLinear()
-    .domain([0, 100])
+    .domain([0, 70])
     .range([height, 0]);
   svg.append("g")
     .call(d3.axisLeft(y).tickFormat((d) => (d === 0 ? d : d + "%")));
