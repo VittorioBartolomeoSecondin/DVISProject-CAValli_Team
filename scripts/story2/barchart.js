0957d6dde19f1f7a13c3b780b1f3d23628fe2205
Alessandro Longo <148397539+Ale8599@users.noreply.github.com>
2024-01-12 13:16:38 +0100

    Update barchart.js

--- scripts/story2/barchart.js
+++ scripts/story2/barchart.js
@@ -1,6 +1,6 @@
 // Set up the SVG dimensions
 var margin = { top: 30, right: 70, bottom: 90, left: 100 },
-		        width = 750 - margin.left - margin.right,
+		        width = 1200 - margin.left - margin.right,
 		        height = 650 - margin.top - margin.bottom;
 
 // append the svg object to the body of the page
