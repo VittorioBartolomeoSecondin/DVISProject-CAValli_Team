de77e8f224bc4f8be339c7ca2bd9840f0ead0627
Alessandro Longo <148397539+Ale8599@users.noreply.github.com>
2024-01-12 16:56:58 +0100

    Update barchart.js

--- scripts/story2/barchart.js
+++ scripts/story2/barchart.js
@@ -34,12 +34,12 @@ d3.csv("data/story2/barcharts/barchart_AUT.csv").then( function(data) {
   xAxis.selectAll("text")
     .attr("transform", function(d) {
         const labelWidth = this.getBBox().width;
-        const desiredDistance = 15;
+        const desiredDistance = 7;
         const adjustment = labelWidth > desiredDistance ? -desiredDistance : 0;
         return `translate(${adjustment},0)rotate(-90)`;
     })
     .style("text-anchor", "end")
-    .attr("dy", "2px");
+    .attr("dx", "-2px");
   
   // Add Y axis
   const y = d3.scaleLinear()
