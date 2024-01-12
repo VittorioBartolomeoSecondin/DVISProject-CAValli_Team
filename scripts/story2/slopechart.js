b1acbb32be0422e7fe3fa7a9e822bccce4d79d9e
VittorioBartolomeoSecondin <73950586+VittorioBartolomeoSecondin@users.noreply.github.com>
2024-01-12 16:10:18 +0100

    Update slopechart.js

--- scripts/story2/slopechart.js
+++ scripts/story2/slopechart.js
@@ -104,8 +104,8 @@
         .data(dataset)
         .enter().append("text")
         .attr("class", "start-label")
-        .attr("x", xScale(2013) - 15)
-        .attr("y", function(d) { return yScale(+d['2013']); })
+        .attr("x", xScale(2013) - 20)
+        .attr("y", function(d) { return yScale(+d['2013'] + 5); })
         .text(function(d) { return d.sex; })
         .style("fill", function(d, i) { return (d.sex === "M") ? colors[0] : colors[1]; });
 
