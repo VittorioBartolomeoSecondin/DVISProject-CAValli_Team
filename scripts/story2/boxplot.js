73c944e68d1c5693539538ce79be87bb8cda32dd
VittorioBartolomeoSecondin <73950586+VittorioBartolomeoSecondin@users.noreply.github.com>
2024-01-13 10:59:04 +0100

    Update boxplot.js

--- scripts/story2/boxplot.js
+++ scripts/story2/boxplot.js
@@ -127,7 +127,7 @@ d3.csv("data/story2/boxplot.csv").then(function (data) {
 
     // Show the Y scale
     var y = d3.scaleLinear()
-        .domain([10, 70])
+        .domain([0, 80])
         .range([height, 0])
     svg.append("g").call(d3.axisLeft(y))
 
@@ -145,7 +145,7 @@ d3.csv("data/story2/boxplot.csv").then(function (data) {
         .style("width", 40)
 
     // rectangle for the main box
-    var boxWidth = 10
+    var boxWidth = 25
     svg
         .selectAll("boxes")
         .data(sumstat)
