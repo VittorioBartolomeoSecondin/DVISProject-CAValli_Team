6fd11f2c0dbbef3b6d61c00696ac77a2f37a6d99
VittorioBartolomeoSecondin <73950586+VittorioBartolomeoSecondin@users.noreply.github.com>
2024-01-13 11:13:56 +0100

    Update boxplot.js

--- scripts/story2/boxplot.js
+++ scripts/story2/boxplot.js
@@ -86,7 +86,7 @@ d3.csv("data/story2/boxplot.csv").then(function (data) {
 
 // set the dimensions and margins of the graph
 var margin = { top: 10, right: 30, bottom: 30, left: 80 },
-    width = 600 - margin.left - margin.right,
+    width = 800 - margin.left - margin.right,
     height = 1000 - margin.top - margin.bottom;
 
 // append the svg object to the body of the page
