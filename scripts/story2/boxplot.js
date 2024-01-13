889e82983f43ac98cde4896ddc7e4b27cc9a1b6e
VittorioBartolomeoSecondin <73950586+VittorioBartolomeoSecondin@users.noreply.github.com>
2024-01-13 11:09:29 +0100

    Update boxplot.js

--- scripts/story2/boxplot.js
+++ scripts/story2/boxplot.js
@@ -85,9 +85,9 @@ d3.csv("data/story2/boxplot.csv").then(function (data) {
 });*/
 
 // set the dimensions and margins of the graph
-var margin = { top: 10, right: 50, bottom: 30, left: 40 },
-    width = 400 - margin.left - margin.right,
-    height = 600 - margin.top - margin.bottom;
+var margin = { top: 10, right: 30, bottom: 30, left: 60 },
+    width = 600 - margin.left - margin.right,
+    height = 1000 - margin.top - margin.bottom;
 
 // append the svg object to the body of the page
 var svg = d3.select("#boxplot")
