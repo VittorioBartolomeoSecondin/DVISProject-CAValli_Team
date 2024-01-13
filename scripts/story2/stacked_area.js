016c88ee9cbaf455ae8be467922eb13dad9853cf
Alessandro Longo <148397539+Ale8599@users.noreply.github.com>
2024-01-13 12:52:51 +0100

    Update stacked_area.js

--- scripts/story2/stacked_area.js
+++ scripts/story2/stacked_area.js
@@ -1,43 +1,44 @@
 // Set up the SVG dimensions
 var margin = { top: 30, right: 70, bottom: 90, left: 100 },
-			      width = 750 - margin.left - margin.right,
-			      height = 650 - margin.top - margin.bottom;
+    width = 750 - margin.left - margin.right,
+    height = 650 - margin.top - margin.bottom;
 
 // append the svg object to the body of the page
 const svg = d3.select("#stacked_area")
-  .append("svg")
+    .append("svg")
     .attr("id", "stacked_area_svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
-  .append("g")
+    .append("g")
     .attr("transform",
-          `translate(${margin.left}, ${margin.top})`);
+        `translate(${margin.left}, ${margin.top})`);
 
-//Read the data
-d3.csv("data/story2/disability_neet.csv").then( function(data) {
+// Read the data
+d3.csv("data/story2/disability_neet.csv").then(function (data) {
 
-  // Stack the data
+    // Stack the data
     const stackedData = d3.stack()
         .keys(data.columns.slice(1))
         .order(d3.stackOrderNone)
         .offset(d3.stackOffsetNone)
+        .value((d, key) => +d[key])
         (data);
 
-  // Add X axis
-    const x = d3.scaleBand()
+    // Add Y axis
+    const y = d3.scaleBand()
         .domain(data.map(d => d.type))
-        .range([0, width])
+        .range([height, 0])
         .padding(0.2);
+    svg.append("g")
+        .call(d3.axisLeft(y));
+
+    // Add X axis
+    const x = d3.scaleLinear()
+        .domain([0, d3.max(stackedData[stackedData.length - 1], d => d[1])])
+        .range([0, width]);
     svg.append("g")
         .attr("transform", `translate(0, ${height})`)
         .call(d3.axisBottom(x));
-
-  // Add Y axis
-    const y = d3.scaleLinear()
-        .domain([0, d3.max(stackedData[stackedData.length - 1], d => d[1])])
-        .range([height, 0]);
-    svg.append("g")
-        .call(d3.axisLeft(y));
 
     // Color palette
     const color = d3.scaleOrdinal()
@@ -50,8 +51,8 @@ d3.csv("data/story2/disability_neet.csv").then( function(data) {
         .join("path")
         .style("fill", d => color(d.key))
         .attr("d", d3.area()
-            .x(d => x(d.data.type))
-            .y0(d => y(d[0]))
-            .y1(d => y(d[1]))
+            .y(d => y(d.data.type))
+            .x0(d => x(d[0]))
+            .x1(d => x(d[1]))
         );
 });
