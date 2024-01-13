c00afd44922305a64cfce50c8098503ac22d873d
VittorioBartolomeoSecondin <73950586+VittorioBartolomeoSecondin@users.noreply.github.com>
2024-01-13 18:54:22 +0100

    Update barchart.js

--- scripts/story2/barchart.js
+++ scripts/story2/barchart.js
@@ -1,8 +1,8 @@
 function updateBarChart(selectedYear, selectedCountry) {
 	
 	// Set up the SVG dimensions
-	var margin = { top: 30, right: 50, bottom: 120, left: 50 },
-			        width = 750 - margin.left - margin.right,
+	var margin = { top: 30, right: 25, bottom: 120, left: 25 },
+			        width = 800 - margin.left - margin.right,
 			        height = 650 - margin.top - margin.bottom;
 	
 	// append the svg object to the body of the page
