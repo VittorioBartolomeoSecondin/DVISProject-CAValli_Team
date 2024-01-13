29715886c3675c36132f2fd7fb08a1fbd7f55107
Alessandro Longo <148397539+Ale8599@users.noreply.github.com>
2024-01-13 13:45:16 +0100

    Update stacked_area.js

--- scripts/story2/stacked_area.js
+++ scripts/story2/stacked_area.js
@@ -26,7 +26,7 @@ d3.csv("data/story2/areachart.csv").then( function(data) {
   const stackedData = d3.stack()
     .keys(mygroup)
     .value(function(d, key){
-      return d[1][key].value
+      return d[0][key].value
     })
     (sumstat);
 
