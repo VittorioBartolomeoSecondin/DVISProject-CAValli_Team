14c0bf2359f7ca8be1b3e7d457889f344d24b59d
Alessandro Longo <148397539+Ale8599@users.noreply.github.com>
2024-01-13 16:30:45 +0100

    Update stacked_area.js

--- scripts/story2/stacked_area.js
+++ scripts/story2/stacked_area.js
@@ -44,7 +44,7 @@ d3.csv("data/story2/areachart.csv").then( function(data) {
 
   // Add Y axis
   const y = d3.scaleLinear()
-    .domain([0, 100)
+    .domain([0, 100])
     .range([height, 0]);
   svg.append("g")
     .call(d3.axisLeft(y).tickFormat((d) => (d === 0 ? d : d + "%")));
