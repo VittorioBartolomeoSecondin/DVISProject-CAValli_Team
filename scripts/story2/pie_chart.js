16286c510ed05a4c4a32ce1882a4508d2f4c54ac
Alessandro Longo <148397539+Ale8599@users.noreply.github.com>
2024-01-15 12:31:03 +0100

    Update pie_chart.js

--- scripts/story2/pie_chart.js
+++ scripts/story2/pie_chart.js
@@ -6,20 +6,21 @@ var margin = { top: 30, right: 70, bottom: 90, left: 100 },
 // The radius of the pieplot is half the width or half the height (smallest one). I subtract a bit of margin.
 const radius = Math.min(width, height) / 2 - margin;
 
-// append the svg object to the div called 'my_dataviz'
+// append the svg object
 const svg = d3.select("#pie_chart")
   .append("svg")
+  .attr("id", "pie_chart_svg")
     .attr("width", width)
     .attr("height", height)
   .append("g")
     .attr("transform", `translate(${width/2}, ${height/2})`);
 
-// Create dummy data
-const data = {a: 9, b: 20, c:30, d:8, e:12}
+// Create data
+const data = {a: 7.8, b: 29.8, c:22, d:6.8, e:15.4, f:5.8, g:12.5}
 
 // set the color scale
 const color = d3.scaleOrdinal()
-  .range(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56"])
+  .range(["#259C1F", "#20EACC", "#6608CA", "#EFE50D", "#a05d56", "#FF5733","#FF9E00"])
 
 // Compute the position of each group on the pie:
 const pie = d3.pie()
