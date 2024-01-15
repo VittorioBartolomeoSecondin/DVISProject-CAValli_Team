e2fd7ab1eb1378c85f69a4444893e4b14d2a5ae0
Alessandro Longo <148397539+Ale8599@users.noreply.github.com>
2024-01-15 12:44:23 +0100

    Update pie_chart.js

--- scripts/story2/pie_chart.js
+++ scripts/story2/pie_chart.js
@@ -17,7 +17,6 @@ const svg = d3.select("#pie_chart")
 
 // Create data
 const data = {a: 7.8, b: 29.8, c:22, d:6.8, e:15.4, f:5.8, g:12.5}
-//const data = {a: 7, b: 29, c:22, d:6, e:15, f:5, g:16}
 
 // set the color scale
 const color = d3.scaleOrdinal()
@@ -41,3 +40,37 @@ svg
   .attr("stroke", "black")
   .style("stroke-width", "2px")
   .style("opacity", 0.7)
+  .on("mouseover", function (event, d) {
+            d3.select(this).attr("r", 5);
+
+            /*if (!tooltip) {
+               tooltip = d3.select("body").append("div")
+                  .attr("id", "scatterplot_tooltip")
+                  .attr("class", "tooltip")
+                  .style("opacity", 0);
+            }
+
+            // Show the tooltip
+            tooltip.transition()
+               .duration(200)
+               .style("opacity", 1)
+
+            // Set the customized tooltip content
+            tooltip.html(`<b>${d.Country} (${d.Abbreviation})</b> <br>
+    			       NEETs: ${d.neet}% <br>
+			       Persons at risk of poverty: ${d.poverty}%`)
+               .style("left", (event.pageX + 10) + "px")
+               .style("top", (event.pageY - 20) + "px");*/
+         })
+         .on("mouseout", function (event, d) {
+            d3.select(this).attr("r", 2.5);
+
+            /*if (tooltip) {
+               tooltip.transition()
+                  .duration(200)
+                  .style("opacity", 0)
+                  .remove();
+               tooltip = null; // Reset tooltip variable
+*/
+           //}
+         });
