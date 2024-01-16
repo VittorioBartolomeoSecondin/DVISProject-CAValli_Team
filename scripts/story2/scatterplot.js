3b0dcc26c1fc3917b3e41822836490313c435e6f
Alessandro Longo <148397539+Ale8599@users.noreply.github.com>
2024-01-16 14:09:10 +0100

    Update scatterplot.js

--- scripts/story2/scatterplot.js
+++ scripts/story2/scatterplot.js
@@ -43,11 +43,12 @@ function updateScatterplotChart(selectedValue) {
       // Add X axis label
       svg.append("text")
 		  .attr("class", "axis-label")
-		  .attr("x", width / 2)
+		  .attr("x", width - 20)
 		  .attr("y", height + margin.bottom - 40) // Adjusted the y-coordinate
 		  .style("text-anchor", "middle")
 		  .style("fill", "black")
-		  .text("Young people neither in employment nor in education and training (NEET)");
+	          .style("font-size", 12)
+		  .text("NEETs");
 
       // Add Y axis
       const y = d3.scaleLinear()
