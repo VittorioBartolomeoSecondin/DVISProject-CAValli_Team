d28876c2d97f7209927d2640bf083d3a800cfd0d
VittorioBartolomeoSecondin <73950586+VittorioBartolomeoSecondin@users.noreply.github.com>
2024-01-16 13:32:47 +0100

    Update butterfly.js

--- scripts/story1/butterfly.js
+++ scripts/story1/butterfly.js
@@ -59,12 +59,12 @@ const Butterfly = {
 
          var yAxisF = g => g
             .attr("transform", `translate(${xF(0) + 50}, 0)`)
-            .call(d3.axisRight(y).tickSize(-10).tickFormat(''))
+            .call(d3.axisRight(y).tickSize(-5).tickFormat(''))
             .call(g => g.selectAll(".tick line").attr("stroke", "black"));
 
          var yAxisM = g => g
             .attr("transform", `translate(${xM(0) - 50}, 0)`)
-            .call(d3.axisRight(y).tickSize(10).tickFormat(''))
+            .call(d3.axisRight(y).tickSize(5).tickFormat(''))
             .call(g => g.selectAll(".tick line").attr("stroke", "black"));
 
          // Append the rectangles without width initially
