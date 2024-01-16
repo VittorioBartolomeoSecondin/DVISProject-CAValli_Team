f21b5ddf8a262f95ccd74213eacc55257a2c4218
Alessandro Longo <148397539+Ale8599@users.noreply.github.com>
2024-01-16 16:30:01 +0100

    Update barcharts.js

--- scripts/story1/barcharts.js
+++ scripts/story1/barcharts.js
@@ -67,6 +67,16 @@ const Barcharts = {
                .attr("fill", "black")
                .style("text-anchor", "end");
 
+            // Add X axis label
+            svg.append("text")
+      		  .attr("class", "axis-label")
+      		  .attr("x", width - 20)
+      		  .attr("y", height + margin.bottom - 40) // Adjusted the y-coordinate
+      		  .style("text-anchor", "middle")
+      		  .style("fill", "black")
+      	     .style("font-size", 8)
+      		  .text("NEETs");
+
             // Add Y axis
             let y = d3.scaleBand()
                .range([height, 0])
